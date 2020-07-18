e6d4baf5b79dae18afcf6e9276d51d2275857e13
htam00 <anonimozurik@gmail.com>
2020-07-18 00:30:56 -0300

    refactory code...

--- js/bundle.js
+++ js/bundle.js
@@ -1,8 +1,31 @@
-function openNavbar() {
-	let x = document.getElementById("header")
-	if(x.className === "header") {
-		x.className += " responsive"
-	} else {
-		x.className = "header"
-	}
+let headerComponent = document.getElementById("header")
+
+/* This function open the menu lateral.
+ *
+ * function   open
+ * @Component Menu
+ */
+const openMenu = () => {
+	return headerComponent.className += " responsive"
 }
+
+/* This function Closed the menu lateral.
+ *
+ * function   closed
+ * @Component Menu
+ */
+const closedMenu = () => {
+	return headerComponent.className = "header"
+}
+
+// Toggle Menu [ open || close ]
+let elementToOpenMenu = document.getElementById("open-menu"),
+elementToClosedMenu = document.getElementById("closed-menu")
+
+// Toggle Theme [ light || dark ]
+let lightTheme = document.getElementById("light-theme"), 
+darkTheme = document.getElementById("dark-theme")
+
+// Events Components
+elementToOpenMenu.addEventListener("click", openMenu)
+elementToClosedMenu.addEventListener("click", closedMenu)
